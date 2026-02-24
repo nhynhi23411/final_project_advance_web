f2095331ba55516f7ca825c6dbf10acfb872fa48
quynhnnn016 <quynhnnn23410@st.uel.edu.vn>
2026-02-24 11:07:22 +0700

    fix: remove nested .git and add client-site source code

---
+++ client-site/src/app/app.module.ts
@@ -0,0 +1,57 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+
+// layouts
+import { AuthComponent } from "./layouts/auth/auth.component";
+
+// auth views
+import { LoginComponent } from "./views/auth/login/login.component";
+import { RegisterComponent } from "./views/auth/register/register.component";
+
+// no layouts views
+import { IndexComponent } from "./views/index/index.component";
+import { LandingComponent } from "./views/landing/landing.component";
+import { ProfileComponent } from "./views/profile/profile.component";
+
+// components for views and layouts
+
+import { FooterComponent } from "./components/footers/footer/footer.component";
+import { FooterSmallComponent } from "./components/footers/footer-small/footer-small.component";
+import { HeaderStatsComponent } from "./components/headers/header-stats/header-stats.component";
+import { IndexNavbarComponent } from "./components/navbars/index-navbar/index-navbar.component";
+import { MapExampleComponent } from "./components/maps/map-example/map-example.component";
+import { IndexDropdownComponent } from "./components/dropdowns/index-dropdown/index-dropdown.component";
+import { TableDropdownComponent } from "./components/dropdowns/table-dropdown/table-dropdown.component";
+import { PagesDropdownComponent } from "./components/dropdowns/pages-dropdown/pages-dropdown.component";
+import { NotificationDropdownComponent } from "./components/dropdowns/notification-dropdown/notification-dropdown.component";
+import { UserDropdownComponent } from "./components/dropdowns/user-dropdown/user-dropdown.component";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    IndexDropdownComponent,
+    PagesDropdownComponent,
+    TableDropdownComponent,
+    NotificationDropdownComponent,
+    UserDropdownComponent,
+    FooterComponent,
+    FooterSmallComponent,
+    HeaderStatsComponent,
+    MapExampleComponent,
+    AuthComponent,
+    IndexNavbarComponent,
+    AuthComponent,
+    LoginComponent,
+    RegisterComponent,
+    IndexComponent,
+    LandingComponent,
+    ProfileComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
